8c4e84b738797420e51c85892fc33422d65f574b
Pawan1618 <148877397+Pawan1618@users.noreply.github.com>
2026-07-12 20:04:55 +0530

    Time: 81 ms (10.3%), Space: 43.5 MB (75.05%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -4,11 +4,13 @@ public:
         int n=arr.size();
         vector<int>temp=arr;
         sort(arr.begin(),arr.end());
-        arr.erase(unique(arr.begin(), arr.end()), arr.end());
+        // arr.erase(unique(arr.begin(), arr.end()), arr.end());
         map<int,int>mp;
+        int cnt=1;
         for(int i=0;i<n;i++){
             if (mp.find(arr[i]) == mp.end()) {
-            mp[arr[i]] = i + 1;
+            mp[arr[i]] =cnt;
+            cnt++;
          }
         }
         for(int i=0;i<n;i++){
