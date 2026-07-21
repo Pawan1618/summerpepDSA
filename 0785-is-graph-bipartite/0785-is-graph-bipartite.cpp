c738a71ccd7a785aad8a6a41d499395174b14c50
Pawan1618 <148877397+Pawan1618@users.noreply.github.com>
2026-07-21 15:49:15 +0530

    Time: 0 ms (100%), Space: 17.5 MB (58.67%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        
-        int n = graph.size();
-        vector<int> color(n, -1);   // -1 = uncolored
-        
-        for(int i = 0; i < n; i++) {
-            
-            if(color[i] == -1) {
-                
-                queue<int> q;
-                q.push(i);
-                color[i] = 0;
-                
-                while(!q.empty()) {
-                    
-                    int node = q.front();
-                    q.pop();
-                    
-                    for(auto neighbor : graph[node]) {
-                        
-                        if(color[neighbor] == -1) {
-                            color[neighbor] = 1 - color[node];
-                            q.push(neighbor);
-                        }
-                        
-                        else if(color[neighbor] == color[node]) {
-                            return false;
-                        }
-                    }
-                }
+    bool dfs(int node, int color, vector<int>& vis, vector<vector<int>>& graph) {
+        vis[node] = color;
+
+        for (int neighbour : graph[node]) {
+
+            // If not colored, color with opposite color
+            if (vis[neighbour] == 0) {
+                int nextColor = (color == 1) ? 2 : 1;
+
+                if (!dfs(neighbour, nextColor, vis, graph))
+                    return false;
+            }
+
+            // If already colored with same color
+            else if (vis[neighbour] == vis[node]) {
+                return false;
             }
         }
-        
+
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> vis(n, 0);   // 0 = unvisited, 1 = Color A, 2 = Color B
+
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == 0) {
+                if (!dfs(i, 1, vis, graph))
+                    return false;
+            }
+        }
+
         return true;
     }
 };
