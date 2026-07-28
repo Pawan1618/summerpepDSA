b92d183153e5980d39f0a94f3236f96aa13d4b90
Pawan1618 <148877397+Pawan1618@users.noreply.github.com>
2026-07-28 16:25:26 +0530

    Time: 0 ms (100%), Space: 19.7 MB (54.3%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,24 +1,63 @@
-class Solution {
+class DSU {
+    vector<int> parent;
+    vector<int> size;
+
 public:
-    void dfs(int i,vector<int>&vis,vector<vector<int>>&isConnected,int n){
-        vis[i]=1;
-        for(int j=0;j<n;j++){
-            if(!vis[j]&&isConnected[i][j]==1){
-                dfs(j,vis,isConnected,n);
-            }
+    DSU(int n) {
+        parent.resize(n);
+        size.resize(n, 1);
+
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v) {
+        int pu = findUPar(u);
+        int pv = findUPar(v);
+
+        if (pu == pv)
+            return;
+
+        if (size[pu] < size[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        } else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
         }
     }
+};
+
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        int cnt=0;
-        vector<int>vis(n,0);
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                cnt++;
-                dfs(i,vis,isConnected,n);
+
+        int n = isConnected.size();
+
+        DSU dsu(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i != j && isConnected[i][j]) {
+                    dsu.unionBySize(i, j);
+                }
             }
         }
-        return cnt;
-        
+
+        unordered_set<int> components;
+
+        for (int node = 0; node < n; node++) {
+            components.insert(dsu.findUPar(node));
+        }
+
+        return components.size();
     }
 };
