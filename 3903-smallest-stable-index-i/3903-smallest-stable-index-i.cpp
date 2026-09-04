c09c3c41dfde5225a9c76342e9bbfc1b00a6136f
Pawan1618 <148877397+Pawan1618@users.noreply.github.com>
2026-09-04 15:47:00 +0530

    Time: 56 ms (5.31%), Space: 31.4 MB (36.87%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>maxi(n);
+        vector<int>mini(n);
+        int mx=nums[0];
+        int mn=nums[n-1];
+        for(int i=0;i<n;i++){
+            if(nums[i]>=mx){
+                mx=nums[i];
+            }
+            maxi[i]=mx;
+            // mini[i]=mn;
+        }
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]<=mn){
+                mn=nums[i];
+            }
+            mini[i]=mn;
+        }
+        for(int i:mini){
+            cout<<i;
+        }
+        cout<<endl;
+        for(int i:maxi)cout<<i;
+        for(int i=0;i<n;i++){
+            if((maxi[i]-mini[i])<=k)return i;
+        }
+        return -1;
+
+        
+    }
+};
